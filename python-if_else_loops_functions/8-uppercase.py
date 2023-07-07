9ae2a822799bf16d5b3ce5722618481f4bcbe739
GChukwudi <g.chukwudi@alustudent.com>
2023-07-07 00:30:05 -0700

    printing lowercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        print("{}".format(str[:i+1]))
+offset = -32
+
+def uppercase(stru):
+    i = 0
+    while i < len(stru):
+        if islower(stru[i]):
+            holder = ord(stru[i]) + offset
+            new_str = chr(holder)
+        else:
+            new_str = stru[i]
+            print("{}".format(new_str), end='')
+            i = i + 1
+            print("")
+
+def islower(strs):
+    value = ord(strs)
+    if value >= 97 and value <= 122:
+        return True
+    else:
+        return False
