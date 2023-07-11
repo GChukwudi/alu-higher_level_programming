eaa0a5bf46fb87fbad6ee93275e2fde731bcfdb9
GChukwudi <g.chukwudi@alustudent.com>
2023-07-11 00:48:36 -0700

    printing uppercase file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-            print("")
+    for a in str:
+        C = ord(a) > 96 and ord(a) < 123
+        print("{}".format(chr(ord(a) - 32 if C else ord(a))), end="")
+    print()
