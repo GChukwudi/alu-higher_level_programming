2222cd3843f2c2d12e57553ab079ce27c5f70fdc
GChukwudi <g.chukwudi@alustudent.com>
2023-07-16 23:25:49 -0700

    removing characters of c and C

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string = ''
-    for i in my_string:
-        if (i.lower()) == 'c':
-            continue
-        string += i
-        return string
+    no_c = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(no_c))
