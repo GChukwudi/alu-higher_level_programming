7de3a5db275d4d8c7d8fb6453304fac54b1105f3
GChukwudi <g.chukwudi@alustudent.com>
2023-07-17 00:37:11 -0700

    Divisible by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return None
     new = my_list[:]
-    for i inn range(len(my_list)):
+    for i in range(len(my_list)):
         if new[i] % 2 == 0:
             new[i] = True
         else:
