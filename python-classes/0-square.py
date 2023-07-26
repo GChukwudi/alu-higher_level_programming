b03142862dc0eef1c739e3857f5dee94706e1e76
GChukwudi <g.chukwudi@alustudent.com>
2023-07-25 23:49:00 -0700

    my first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,5 +3,11 @@
 
 
 class Square:
-     """ A class representing a square"""
+     """ A class representing a square
+      Attributes:
+        side_length (int or float): The length of each side of the square.
+
+    Methods:
+        calculate_area(): Calculate the area of the square.
+        calculate_perimeter(): Calculate the perimeter of the square."""
     pass
