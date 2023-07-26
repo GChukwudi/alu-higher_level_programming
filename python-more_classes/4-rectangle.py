5ef7f5ad7ac320cec60019fd7e9ec11c3e09e0fe
GChukwudi <g.chukwudi@alustudent.com>
2023-07-26 09:51:38 -0700

    updated

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -9,16 +9,19 @@ class Rectangle:
         """ Instantiation with optional width and height"""
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """ width
         """
         return self.__width
+
     @property
     def height(self):
         """ height
         """
         return self.__height
+
     @width.setter
     def width(self, value):
         """ width setter
@@ -38,14 +41,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """ returns rectangle area"""
         return self.__width * self.__height
+
     def perimeter(self):
         """ returns rectangle perimiter"""
         if self.__width is 0 or self.__height is 0:
             return 0
         return self.__width * 2 + self.__height * 2
+
     def __str__(self):
         """ return the rectangle with the character #
         """
@@ -53,6 +59,7 @@ class Rectangle:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))
+
     def __repr__(self):
         """ return a string representation of the rectangle
         """
