879cd6a31647bc2f9a7213507e541af5f7615843
GChukwudi <g.chukwudi@alustudent.com>
2023-07-26 00:23:48 -0700

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class Square. """
+
+
+class Square:
+    """ defining a square. """
+    def __init__(self, size=0):
+        """defining size and validation"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
