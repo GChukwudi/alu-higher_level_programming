7c29c3c5d999b661f152351e0ca8e678ffc098fb
GChukwudi <g.chukwudi@alustudent.com>
2023-07-25 23:59:01 -0700

    Square with size:

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square. """
+
+
+class Square:
+    """ defining a square. """
+    def __int__(self, size):
+        """ initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
