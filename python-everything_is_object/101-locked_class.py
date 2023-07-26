624e2bd2ff8756486c43064a90494a9b9f01162a
GChukwudi <g.chukwudi@alustudent.com>
2023-07-26 11:27:50 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" class LockedClass with no class or object attribute
+""" LockedClass
 """
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    __slots__= ['first_name']
