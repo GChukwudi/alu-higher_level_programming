a42cbee2be36a2f9caed3774f58aaf2270c623b2
root <godsfavourchukwudi21@gmail.com>
2023-10-05 07:07:59 -0700

    Star wars Wedge Antilles

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+let def = {};
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+  if (error) {
+    console.log(error);
+  } else {
+    const abc = JSON.parse(body);
+    abc.characters.forEach(function (item, index, array) {
+      request(item, function (error, response, content) {
+        if (error) {
+          console.log(error);
+        } else {
+          def = JSON.parse(content);
+          console.log(def.name);
         }
       });
     });
